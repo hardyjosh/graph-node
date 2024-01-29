289aae3be3a6a091c19438dafa3a6c6d9d3c28d7
Josh <jks.hardy@gmail.com>
2024-01-29 21:06:20 +0000

    config

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Replace the placeholder with the actual bearer token
-sed -i "s/$BEARER_TOKEN/${BEARER_TOKEN}/g" /etc/nginx/conf.d/graph-node-8020.conf
-sed -i "s/$BEARER_TOKEN/${BEARER_TOKEN}/g" /etc/nginx/conf.d/graph-node-5001.conf
+sed -i "s/BEARER_TOKEN/${BEARER_TOKEN}/g" /etc/nginx/conf.d/graph-node-8020.conf
+sed -i "s/BEARER_TOKEN/${BEARER_TOKEN}/g" /etc/nginx/conf.d/graph-node-5001.conf
 
 # Start Nginx in the foreground
 exec nginx -g 'daemon off;'
