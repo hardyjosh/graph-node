022a358636d3d492f72ab923b816f7d94919d784
Josh <jks.hardy@gmail.com>
2024-01-29 20:45:10 +0000

    nginx wasn't running

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,5 +3,5 @@
 # Replace the placeholder with the actual bearer token
 sed -i "s/$BEARER_TOKEN/${BEARER_TOKEN}/g" /etc/nginx/conf.d/graph-node-8020.conf
 
-# Execute the original entrypoint (Docker CMD)
-exec "$@"
+# Start Nginx in the foreground
+exec nginx -g 'daemon off;'
